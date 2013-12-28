739660ba9c219056862755baa1066abedd2b9569
Jay Kid <cyph3rfox@gmail.com>
2013-12-28 23:09:00 +0100

    added Torrents buttons, refactor pending

--- index.js
+++ index.js
@@ -5,5 +5,7 @@ var requestHandlers = require('./requestHandlers');
 var handle = {}
 handle['/ftp/start'] = requestHandlers.startFTP;
 handle['/ftp/stop'] = requestHandlers.stopFTP;
+handle['/torrents/start'] = requestHandlers.startTorrents;
+handle['/torrents/stop'] = requestHandlers.stopTorrents;
 
 server.iniciar(router.route,handle);
--- requestHandlers.js
+++ requestHandlers.js
@@ -14,6 +14,39 @@ function stopFTP() {
     console.log("FTP Server stopped");
 }
 
+function startTorrents() {
+    
+    handleTorrents('start');
+    console.log("Torrents Server started");
+}
+
+function stopTorrents() {
+
+    handleTorrents('stop');
+    console.log("Torrents Server stopped");
+}
+
+function handleTorrents(action)
+{
+    var transmision_bin = '/etc/init.d/transmission-daemon';
+    var spawn       = require('child_process').spawn;
+    var command     = spawn(transmision_bin, [action]);
+
+    command.stdout.on('data', function (data) {
+        console.log('stdout: '+data);
+    });
+
+    command.stderr.on('data', function (data) {
+        console.log('stderr: '+data);
+    });
+
+    command.on('close', function (code) {
+        if (code !== 0) {
+            console.log('grep_cmd process exited with code ' + code);
+        }
+    });
+}
+
 function startService(command_name)
 {
     handleService(command_name, 'start');
@@ -24,11 +57,11 @@ function stopService(command_name)
     handleService(command_name, 'stop');
 }
 
-function handleService(command_name, mode)
+function handleService(command_name, action)
 {
     var SERVICE_CMD = 'service';
     var spawn       = require('child_process').spawn;
-    var command     = spawn(SERVICE_CMD, [command_name, mode]);
+    var command     = spawn(SERVICE_CMD, [command_name, action]);
 
     command.stdout.on('data', function (data) {
         console.log('stdout: '+data);
@@ -47,3 +80,5 @@ function handleService(command_name, mode)
 
 exports.startFTP = startFTP;
 exports.stopFTP = stopFTP;
+exports.startTorrents = startTorrents;
+exports.stopTorrents = stopTorrents;
